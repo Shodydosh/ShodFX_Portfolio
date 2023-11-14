74288892ddadbae965a5c27f3339ac2e828eb18b
Tung Nguyen <shodydosh@gmail.com>
2023-11-14 23:15:13 +0700

    feat: little Logo adjustment

--- components/logo/Logo.jsx
+++ components/logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import brandLight from './logo.png';
-import brandDark from './logob.png';
+import brandDark from './logo.png';
 import Image from "next/image";
 
 const MainBrand = styled.div`
